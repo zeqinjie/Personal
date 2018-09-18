395a1f4eb5bed56c28c03704e5f01fdb9a491e19
郑泽钦 <zhengzeqin@addcn.com>
2018-09-18 18:45:56 +0800

    RAMAnimatedTabBarController

--- ZQNews/ZQNews/Modules/MainModules/MainTabBarViewController.swift
+++ ZQNews/ZQNews/Modules/MainModules/MainTabBarViewController.swift
@@ -13,30 +13,44 @@ class MainTabBarViewController: RAMAnimatedTabBarController {
 
     // MARK: - LifeCycle
     override func viewDidLoad() {
+        creatChildVCS()
         super.viewDidLoad()
-
     }
 
+    
+}
+    
+extension MainTabBarViewController {
+    // MARK: - <##>Pirvate Method
+    private func creatChildVCS(){
+        //首页
+        addChildVC(childVC: HomeViewController(), title: "首页", image: UIImage(imageLiteralResourceName: "home"), selecteImage: UIImage(named: "home_sel"))
+        //News
+        addChildVC(childVC: NewsViewController(), title: "新闻", image: UIImage(imageLiteralResourceName: "news"), selecteImage: UIImage(named: "news_sel"))
+        //我的
+        addChildVC(childVC: MineViewController(), title: "我的", image: UIImage(imageLiteralResourceName: "user"), selecteImage: UIImage(named: "user_sel"))
+        //设置
+        addChildVC(childVC: SettingViewController(), title: "设置", image: UIImage(imageLiteralResourceName: "settings"), selecteImage: UIImage(named: "settings_sel"))
+    }
+    
     ///添加一个控制器
-    private func addOneChildVC(childVC: UIViewController, title: String?, image: UIImage?, selecteImage: UIImage?) {
+    private func addChildVC(childVC: UIViewController, title: String?, image: UIImage?, selecteImage: UIImage?) {
         
         //1.添加子控制器
         let navVC = UINavigationController(rootViewController: childVC)
         addChildViewController(navVC)
         //2.添加标题
         let item = RAMAnimatedTabBarItem(title: title, image: image, selectedImage: selecteImage)
-        
-//        let animation = RAMTransitionItemAnimations()
-//        animation.iconSelectedColor = UIColor.orange
-//        animation.transitionOptions = UIViewAnimationOptions.transitionCurlUp
-//        item.animation = animation
-        
-        
-        
+        let animation = RAMBounceAnimation()
+        item.animation = animation
+        itemColor(unselColor: ZQColor_888888!, selColor: ZQColor_ff4000!, item: item, animation: animation)
         navVC.tabBarItem = item
-        
-        
+    }
+    
+    private func itemColor(unselColor:UIColor,selColor:UIColor,item:RAMAnimatedTabBarItem,animation:RAMItemAnimation){
+        item.textColor = unselColor
+        item.iconColor = unselColor
+        animation.iconSelectedColor = selColor
+        animation.textSelectedColor = selColor
     }
 }
-    
-
