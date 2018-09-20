44a009dba41acde34724b44b53c0d3fd22acae56
郑泽钦 <zhengzeqin@addcn.com>
2018-09-20 18:48:42 +0800

    +++

---
+++ ZQNews/ZQNews/Tool/ZQTool.swift
@@ -0,0 +1,13 @@
+//
+//  ZQTool.swift
+//  ZQNews
+//
+//  Created by zhengzeqin on 2018/9/20.
+//  Copyright © 2018年 zhengzeqin. All rights reserved.
+//
+
+import UIKit
+
+class ZQTool: NSObject {
+
+}
