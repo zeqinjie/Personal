6d6c9b0429aec85a6f135098491d7a0f10c6be1a
zhengzeqin <1875193628@qq.com>
2018-10-14 21:25:28 +0800

    +++

--- Photoshop/Photoshop/Modules/HomeModules/Home/VC/ZQHomeViewController.swift
+++ Photoshop/Photoshop/Modules/HomeModules/Home/VC/ZQHomeViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ZQHomeViewController: BaseViewController {
 
+    @IBOutlet weak var chooseBtn: UIButton!
+    
+    // MARK: - Override <##>
     override func viewDidLoad() {
         super.viewDidLoad()
+        creatUI()
     }
     
+    override func defaultSet() {
+        super.defaultSet()
+        self.isHideNavigationBar = true
+    }
+    
+    // MARK: - Action
+    @IBAction func chooseAction(_ sender: UIButton) {
+        
+    }
+}
+
+extension ZQHomeViewController {
+    fileprivate func creatUI() {
+        self.chooseBtn.layer.borderColor = ZQColor_563885?.cgColor
+        self.chooseBtn.layer.cornerRadius = 0.5 * self.chooseBtn.frame.width
+        self.chooseBtn.layer.borderWidth = 8
+    }
 }
 
